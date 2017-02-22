9db34939c09ade3c6c00bf9f7b417b5fc5ec7cb1
deepu5g <gautamshri555@gmail.com>
2017-02-22 14:44:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -19,6 +19,10 @@ app.get('/article2', function(req,res){
 res.sendFile(path.join(__dirname, 'ui', 'article2.html'));
 
 });
+app.get('/ui/style.css', function(req,res){
+res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+
+});
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
