e6be1cd849789fe59ecc825f9c0961a1a4d0199a
deepu5g <gautamshri555@gmail.com>
2017-02-25 11:15:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 //counter code
 var button=document.getElementById('counter');
 var counter=0;
-button.oneclick-function(){
+button.oneclick=function(){
   //make a request to counter end point
   //capture a response and store it in a varable
   //render the variable is the correct span
