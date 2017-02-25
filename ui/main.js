17450876d513da730aa4d15449f3f68d29a30da3
deepu5g <gautamshri555@gmail.com>
2017-02-25 15:13:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,9 +4,24 @@ var counter = 0;
 
 button.oneclick = function () {
   //make a request to counter end point
+  var request = new XMLhttpRequest();
   //capture a response and store it in a varable
+request.onreadystatechange= function () {
+     if (request.readystate ===  XMLhttpRequest.DONE) {
+    //take  some action
+    if(request.status===200) {
+        var counter = request.responseText;
+
   //render the variable is the correct span
   counter = counter + 1;
   var span = document.getElementById('count');
   span.innerHTML = counter.toString();
+    }
+    
+}
 };
+//make a request
+request.open('GET','http://http://deepu5g.imad.hasura-app.io/counter', true);
+request.send(null);
+};
+
