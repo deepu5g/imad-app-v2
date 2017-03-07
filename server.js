ddd45f82fe8744b59630a44a093cfa72933a49f7
deepu5g <gautamshri555@gmail.com>
2017-03-07 15:09:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ var config = {
     user:'deepu5g',
     database:'deepu5g' ,
     host:'db.imad.hasura-app.io' ,
-}
+    port:'5432' ,
+    password: process.env.DB_PASSWORD
+};
 var app = express();
 app.use(morgan('combined'));
 
@@ -22,8 +24,17 @@ app.get('/counter', function(req,res){
  counter=counter + 1;
  res.send(counter.toString());
 });
+var pool = new Pool(config) ;
 app.get('/test-db', function (req, res){
-    
+    pool.query('SELECT* FROM test', function (err, result) {
+        if (err) {
+            res.status(500).send(err.tostring()) ;
+        } else {
+            res.send(JSON.stringify(result)) ;
+        }
+    });
+
+
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
