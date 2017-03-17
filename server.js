cbf0d5eafa0c18d231126accc85a663c8eb8b9cf
deepu5g <gautamshri555@gmail.com>
2017-03-17 14:22:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var Pool= require('pg'), Pool;
+
 
 var config = {
     user:'deepu5g',
@@ -24,18 +24,7 @@ app.get('/counter', function(req,res){
  counter=counter + 1;
  res.send(counter.toString());
 });
-var pool = new Pool(config) ;
-app.get('/test-db', function (req, res){
-    pool.query('SELECT * FROM test', function (err, result) {
-        if (err) {
-            res.status(500).send(err.toString()) ;
-        } else {
-            res.send(JSON.stringify(result)) ;
-        }
-});
 
-
-});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
