13438e7b0d99bb1ab05c7e57ffa49787fe9743e9
deepu5g <gautamshri555@gmail.com>
2017-03-29 10:58:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,20 +20,7 @@ request.onreadystatechange= function () {
     
 }
 };
-var sunSize = 30;
-noStroke();
-// the beautiful blue sky
-background(82, 222, 240);
 
-draw = function() {
-    // The sun
-    fill(255, 204, 0);
-    ellipse(200, 298, sunSize, sunSize);
-
-    // The land
-    fill(76, 168, 67);
-    rect(0, 300, 400, 100);
-};
 
 
 //make a request
