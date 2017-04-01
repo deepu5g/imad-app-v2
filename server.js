9fe558e7ef34a8f3ce20947f4b0e1d76222e0c69
deepu5g <gautamshri555@gmail.com>
2017-04-01 16:15:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -48,10 +48,6 @@ var port = 8080; // Use 8080 for local development because you might already hav
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
-var myString = deepak;
-var myNumber = 25;
-var myBoolean = gautam;
-
 
 // Do not edit the code under this line
 console.log("Name: " + myString);
